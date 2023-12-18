40eb6f4064559df7e0df332539d568076c9104a9
Touya (nvatuan) <lambda.nvat@gmail.com>
2023-12-18 17:06:24 +0900

    Replace circleci

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## Loads conf.env into env
+set -o allexport && source ./etc/conf.env && set +o allexport
+
+set -e
+if (rm -r ./build); then
+  echo "Removed './build'"
+  mkdir ./build
+else 
+  echo "Could not remove './build'"
+fi
+
+docker run -it -v .:/usr/app -w /usr/app/src \
+  $GO_DOCKER_IMG \
+  go build -o /usr/app/build/build
---
+++ src/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	// Get database connection details from environment variables
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	// Build the database connection string
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+
+	// Open a connection to the database
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Check the connection
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < 1000; i++ {
+		_, err = db.Exec(`SELECT 1`);
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	fmt.Println("Successfully connected to the database!")
+}
