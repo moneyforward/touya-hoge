4ccf6341fafa448d0a9a2c234fc7214cd2d35e2e
Touya (nvatuan) <lambda.nvat@gmail.com>
2023-12-18 22:56:19 +0900

    Add redis and parallel

--- src/main.go
+++ src/main.go
@@ -8,7 +8,7 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func main() {
+func connectMysql() {
 	// Get database connection details from environment variables
 	dbHost := os.Getenv("DB_HOST")
 	dbPort := os.Getenv("DB_PORT")
@@ -32,7 +32,7 @@ func main() {
 		log.Fatal(err)
 	}
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 100000; i++ {
 		_, err = db.Exec(fmt.Sprintf(`SELECT %v * %v`, i, i));
 		if err != nil {
 			log.Fatal(err)
@@ -40,4 +40,38 @@ func main() {
 	}
 
 	fmt.Println("Successfully connected to the database!")
+}
+
+func connectRedis() {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("%v:%v", os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT")),
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	fmt.Println("Successfully connected to Redis!")
+}
+
+func main() {
+	connectMysql();
+	connectRedis();
 }
